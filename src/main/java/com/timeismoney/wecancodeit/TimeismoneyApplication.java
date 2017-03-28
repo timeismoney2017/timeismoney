31dedd10603a7c6bc044ee39e177a775927d85d7
Paul Yorde <paulyorde26@gmail.com>
2017-03-28 11:22:10 -0400

    setup project

---
+++ src/main/java/com/timeismoney/wecancodeit/TimeismoneyApplication.java
@@ -0,0 +1,12 @@
+package com.timeismoney.wecancodeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeismoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeismoneyApplication.class, args);
+	}
+}
