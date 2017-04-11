dad1bb7c556c62c3dd94591c6954f2988be9bac9
Paul Yorde <paulyorde26@gmail.com>
2017-04-11 19:17:36 -0400

    deleted category

--- src/main/java/com/timeismoney/wecancodeit/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.timeismoney.wecancodeit;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Greeting, Long>{
-
-}
